37ba0b4880bb168b8d0e48e2838762a903ad6e0a
isanmen <eliisanmen@gmail.com>
2024-03-12 21:05:47 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+//alert("Hola este es mi javascript");
+
+let menu_responsive = document.querySelector(".checkbtn");
+menu_responsive.onclick = function () {
+  navBar = document.querySelector(".navbar");
+  navBar.classList.toggle("active");
+};
